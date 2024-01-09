f61161d02eb45d5cb95df503042150c681c4a3d8
Rômulo Nunes <romulotg21@gmail.com>
2024-01-09 10:54:51 -0300

    feat: campoTexto

---
+++ src/components/CampoTexto/CampoTexto.jsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+const LabelEstilizada = styled.label`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 20px;
+  line-heigth: 24px;
+`;
+const InputEstilizado = styled.input`
+  display: block;
+  width: 100%;
+  margin-top: ${(props) => props.theme.spacing.xs};
+  background: ${(props) => props.theme.colors.white};
+  border: 1px solid;
+  border-radius: ${(props) => props.theme.spacing.s};
+  border-color: ${(props) => props.theme.colors.neutral.a};
+  height: 40px;
+  font-height: 400;
+  font-size: 14px;
+  line-height: 17px;
+`;
+
+export const CampoTexto = ({ titulo }) => {
+  return (
+    <LabelEstilizada>
+      {titulo}
+      <InputEstilizado />
+    </LabelEstilizada>
+  );
+};
